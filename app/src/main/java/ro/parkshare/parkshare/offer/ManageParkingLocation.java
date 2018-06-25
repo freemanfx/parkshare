3a1effa7b27e6a89d93b40210bbe060d39c44e87
freemanfx <freemanfx@gmail.com>
2018-06-25 17:53:15 +0300

    add google map

--- app/src/main/java/ro/parkshare/parkshare/offer/ManageParkingLocation.java
+++ app/src/main/java/ro/parkshare/parkshare/offer/ManageParkingLocation.java
@@ -1,18 +1,37 @@
 package ro.parkshare.parkshare.offer;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
 import ro.parkshare.parkshare.R;
+import ro.parkshare.parkshare.service.ParkingLocation;
 
-public class ManageParkingLocation extends AppCompatActivity {
+public class ManageParkingLocation extends AppCompatActivity implements OnMapReadyCallback {
 
+    private GoogleMap map;
+    private ParkingLocation parkingLocation;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_parking_location);
-
         setTitle(getString(R.string.add_parking_location));
+
+        MapFragment mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.map, mMapFragment);
+        fragmentTransaction.commit();
+        mMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
 }
